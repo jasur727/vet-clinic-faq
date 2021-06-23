352d1adb1ad519ffa324fbc850fb563a159e8743
Jasur <developer7102@gmail.com>
2021-06-23 20:43:27 +0200

    add js css html img

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var elsOpenModalButton = document.querySelector('.hero__button');
+var elCloseModalButton = document.querySelector('.exit-button');
+var elModal = document.querySelector('.js-modal');
+
+elsOpenModalButton.addEventListener('click', function(){
+  elModal.classList.add('modal-open')
+});
+
+elCloseModalButton.addEventListener('click', function(){
+elModal.classList.remove('modal-open')
+})
