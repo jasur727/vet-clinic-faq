0ae8ce95575bd64ef8a080f40203337a0da39001
Jasur <developer7102@gmail.com>
2021-06-24 15:15:36 +0200

    add 100%

--- js/main.js
+++ js/main.js
@@ -2,20 +2,21 @@ var elsOpenModalButton = document.querySelector('.hero__button');
 var elCloseModalButton = document.querySelector('.exit-button');
 var elModal = document.querySelector('.js-modal');
 
-elsOpenModalButton.addEventListener('click', function(){
+elsOpenModalButton.addEventListener('click', function () {
   elModal.classList.add('modal-open')
 });
 
-elCloseModalButton.addEventListener('click', function(){
-elModal.classList.remove('modal-open')
+elCloseModalButton.addEventListener('click', function () {
+  elModal.classList.remove('modal-open')
 })
 
-var elsAnsverBtn = document.querySelectorAll('.btn');
-var elsAnsversItem = document.querySelectorAll('.faq__middle--tex');
 
+var elsButton = document.querySelectorAll('.middle--button');
 
-elsAnsverBtn.forEach(function (toggle) {
-  toggle.addEventListener('click', function (){
-    toggle.closest('faq__middle--tex').classList.toggle('ansvers--open')
-  })
+var elModalss = document.querySelectorAll('.faq__contant');
+
+elsButton.forEach(function (btns) {
+  btns.addEventListener('click', function () {
+    btns.closest(".faq__contant").classList.toggle("open-model");
+  });
 })
